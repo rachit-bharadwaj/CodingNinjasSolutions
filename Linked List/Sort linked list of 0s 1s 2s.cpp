e95d735403ec56bb159d28b4fbb0e9aefc690f4d
Rachit Bharadwaj <rachitbharadwaj52@gmail.com>
2022-12-22 21:15:11 +0530

    added name and url of the question

--- Linked List/Sort linked list of 0s 1s 2s.cpp
+++ Linked List/Sort linked list of 0s 1s 2s.cpp
@@ -1,3 +1,6 @@
+// Question Name: Sort linked list of 0s 1s 2s
+// Question link: https://www.codingninjas.com/codestudio/problems/sort-linked-list-of-0s-1s-2s_1071937?leftPanelTab=0
+
 /********************************
     class Node
     {
